0842dfa423451b176bc87a955535f30558410c48
yoaku005 <yoaku005@d.umn.edu>
2018-09-25 11:21:10 -0500

    added a for loop in disemvowel.c to remove vowels from string

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,25 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  int len, i, j;
+  char *result;
+
+
+  len = strlen(str);
+  result = (char*) calloc(len+1, sizeof(char));
+
+  for(i = 0; i < len; i++){
+      if(str[i] != 'a' && str[i] != 'i' && str[i] != 'u' && str[i] != 'e'&& str[i] != 'o'&& \
+          str[i] != 'A' && str[i] != 'I' && str[i] != 'U' && str[i] != 'E'&& str[i] != 'O'){
+            result[j] = str[i];
+            j++;
+          }
+
+  }
+
+  return result;
 }
